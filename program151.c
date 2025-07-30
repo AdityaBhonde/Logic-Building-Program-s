8396c6ec5e64a01f09887e529fc9438bef965ac6
Aditya Bhonde <adityabhonde106@gmail.com>
2025-07-30 15:58:46 +0530

    Add files via upload

---
+++ program151.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// error
+int main()
+{
+    char Arr[50];
+
+    printf("Enter your name : \n"); 
+    scanf("%s",Arr);
+
+    printf("Your name is : %s\n",Arr);  // Space cya nanter ca print nahi hot.
+    
+    return 0;
+}
